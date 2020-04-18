225f7bb2a4b223d87be63746866125eb9d4f5fda
Felix Siebeneicker <felix.sieben@t-online.de>
2020-04-18 18:16:47 +0200

    Adding slime

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    [Header("Animation")]
+    [SerializeField] float speed = 0.25f;
+    [SerializeField] float size = 1f;
+
+    void Update()
+    {
+        HandleIdleAnimation();
+        HandleSizeChange();
+    }
+
+    void HandleIdleAnimation()
+    {
+        float percent = (Mathf.Sin(Time.time * speed) + 1f) * 0.5f;
+        Vector3 scale = Vector3.one * size * Mathf.Lerp(0.995f, 1.05f, percent);
+
+        transform.localScale = scale;
+    }
+
+    void HandleSizeChange()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            size -= 0.15f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            size += 0.15f;
+        }
+    }
+}
