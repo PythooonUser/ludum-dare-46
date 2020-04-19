40417c32a3d0481c35feddd6fe139681f0d650ef
Felix Siebeneicker <felix.sieben@t-online.de>
2020-04-19 15:34:09 +0200

    Adding pause menu

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] CanvasGroup menuGroup = default;
+    [SerializeField] CanvasGroup screenUI = default;
+    [SerializeField] PlayerInputHandler inputHandler = default;
+    [SerializeField] AudioSource audioSource = default;
+
+    bool m_isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (m_isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+            audioSource.Play();
+        }
+    }
+
+    void PauseGame()
+    {
+        m_isPaused = true;
+        Time.timeScale = 0f;
+
+        screenUI.alpha = 0f;
+        menuGroup.alpha = 1f;
+        menuGroup.blocksRaycasts = true;
+        menuGroup.interactable = true;
+
+        inputHandler.SetMenuInteractivity(true);
+    }
+
+    public void ResumeGame()
+    {
+        m_isPaused = false;
+        Time.timeScale = 1f;
+
+        screenUI.alpha = 1f;
+        menuGroup.alpha = 0f;
+        menuGroup.blocksRaycasts = false;
+        menuGroup.interactable = false;
+
+        inputHandler.SetMenuInteractivity(false);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+}
--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -6,14 +6,17 @@ public class PlayerInputHandler : MonoBehaviour
 {
     [SerializeField] float lookSensitivity = 1f;
 
+    bool m_canInteract = true;
+
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        SetMenuInteractivity(false);
     }
 
     public Vector3 GetMoveInput()
     {
+        if (!m_canInteract) { return Vector3.zero; }
+
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         input = Vector3.ClampMagnitude(input, 1f);
         return input;
@@ -21,6 +24,8 @@ public class PlayerInputHandler : MonoBehaviour
 
     public float GetLookInputHorizontal()
     {
+        if (!m_canInteract) { return 0f; }
+
         float input = Input.GetAxisRaw("Mouse X");
         input *= lookSensitivity;
         return input;
@@ -28,8 +33,26 @@ public class PlayerInputHandler : MonoBehaviour
 
     public float GetLookInputVertical()
     {
+        if (!m_canInteract) { return 0f; }
+
         float input = -Input.GetAxisRaw("Mouse Y");
         input *= lookSensitivity;
         return input;
+    }
+
+    public void SetMenuInteractivity(bool activate)
+    {
+        if (activate)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        m_canInteract = !activate;
     }
 }
