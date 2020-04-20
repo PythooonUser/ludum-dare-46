625d7d6228d972f8a10105fed4bb26be7a4d5433
Felix Siebeneicker <felix.sieben@t-online.de>
2020-04-20 18:26:04 +0200

    Adding environment sounds

---
+++ Assets/Scripts/EnvironmentSoundController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSoundController : MonoBehaviour
+{
+    [SerializeField] AudioClip[] clips = default;
+    [SerializeField] Vector2 timeBetweenClips = new Vector2(3f, 6f);
+    [SerializeField] AudioSource audioSource = default;
+
+    float m_timeSinceLastClip = 0f;
+    float m_timeUntilNextClip = 0f;
+
+    void Start()
+    {
+        m_timeUntilNextClip = Random.Range(timeBetweenClips.x, timeBetweenClips.y);
+    }
+
+    void Update()
+    {
+        if (m_timeSinceLastClip >= m_timeUntilNextClip)
+        {
+            AudioClip clip = clips[Random.Range(0, clips.Length)];
+            audioSource.clip = clip;
+            audioSource.Play();
+
+            m_timeSinceLastClip = 0f;
+            m_timeUntilNextClip = clip.length + Random.Range(timeBetweenClips.x, timeBetweenClips.y);
+        }
+        else
+        {
+            m_timeSinceLastClip += Time.deltaTime;
+        }
+    }
+}
